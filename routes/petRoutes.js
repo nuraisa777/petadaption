bbfdcec0253a373608a06493124dcdd17a2ec8be
nuraisa777 <nuraisa.hassan@neu.edu.ph>
2025-11-22 00:14:33 +0800

    updated

--- routes/petRoutes.js
+++ routes/petRoutes.js
@@ -60,6 +60,7 @@ router.get('/search', petController.searchPets);
  *               gender: { type: string }
  *               status: { type: string }
  *               description: { type: string }
+ *               pictureURL: { type: string }
  *     responses:
  *       201:
  *         description: Pet created
